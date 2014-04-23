8e469d39aa62eba2f941671a725ecb71e69eb822
zo.dennis <zo.dennis@98d6372e-d830-f089-a6f9-c0d0c0be48a6>
2014-04-23 15:31:28 +0000

    inputNewNumberToData in Model2048.java

---  java2014/Java2014/src/game2048/Model2048.java
+++  java2014/Java2014/src/game2048/Model2048.java
@@ -1,6 +1,8 @@
 package game2048;
 
 import java.util.Observable;
+import java.util.Random;
+import java.util.Stack;
 
 import model.Model;
 
@@ -8,6 +10,23 @@ public class Model2048 extends Observable implements Model {
 
 	private int[][] data;
 	private int [][] oneStepBeforeData;
+	
+	public void inputNewNumberToData(){
+		Stack<String> stack = new Stack<String>();
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if (data[i][j] == 0) {
+					stack.push(i + "," + j);
+				}
+			}
+		}
+		Random rn = new Random();
+		int answer = rn.nextInt(stack.size());
+		int [] strAnser={Integer.parseInt(stack.get(answer).split(",")[0]),Integer.parseInt(stack.get(answer).split(",")[1])};
+		
+		data[strAnser[0]][strAnser[1]]=2;	
+	}
+	
 	
 	@Override
 	public void moveUp() {
