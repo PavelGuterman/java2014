430d53a5656a964ae27b03cc45c19d0d5ef8690d
pawow2@gmail.com <pawow2@gmail.com@98d6372e-d830-f089-a6f9-c0d0c0be48a6>
2014-05-22 06:58:07 +0000

    Minimax+alfabeta

---
+++ trunk/ java2014/Server2048/src/solver/StateTree.java
@@ -0,0 +1,23 @@
+package solver;
+
+import java.util.ArrayList;
+
+public class StateTree {
+	
+	private StateTree root = null;
+	private int[][] data;
+	private int score;
+	private ArrayList<StateTree> children = new ArrayList<StateTree>(); 
+
+	
+	public StateTree(int[][] data, int score){
+		this.data = data;
+		this.score = score;
+	}
+
+	public addChild(){
+		
+	}
+
+
+}
