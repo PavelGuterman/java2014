2910d13fc7b69f88dc5709e9405966d7f5dd7acd
pawow2@gmail.com <pawow2@gmail.com@98d6372e-d830-f089-a6f9-c0d0c0be48a6>
2014-05-22 06:58:07 +0000

    Minimax+alfabeta

---  java2014/Server2048/src/controller/Main.java
+++  java2014/Server2048/src/controller/Main.java
@@ -1,26 +1,36 @@
 package controller;
 
-import solver.Solver;
-import model.Server2048;
+import java.io.BufferedReader;
 
+import solver.Board;
+import solver.Solver;
+import model.ClienHandler;
+import model.Server2048;
+import solver.AIsolver;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		Solver solver = new Solver();
-	Server2048 s = new Server2048(6951, solver); 
-	/*new ClienHandler() {
-			@Override
-			public void handleClient(BufferedReader inFromClient) {
-				String line;
-				try {
-					while(!((line= inFromClient.readLine()).equals("exit"))){
-						System.out.println(line);
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
+		
+	Server2048 s = new Server2048(6951, new ClienHandler() {
+		
+		@Override
+		public void handleClient(BufferedReader inFromClient) {
+			// TODO Auto-generated method stub
+			System.out.println("start the Solver");
+			Board board = inFromClient;
+			String line;
+			try {
+				while(!((line= inFromClient.readLine()).equals("exit"))){
+					System.out.println(line);
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		})*/
+			
+		}
+	}); 
+	
+		
 	
 	s.start();
 	Thread.sleep(60*1000);
