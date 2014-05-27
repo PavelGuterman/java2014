faae8139ba08ec6d995545674f511c3c35aabc9c
pawow2@gmail.com <pawow2@gmail.com@98d6372e-d830-f089-a6f9-c0d0c0be48a6>
2014-05-27 11:44:59 +0000

    Maze Solver

--- trunk/ java2014/Server2048/src/solver/MazeSolver.java
+++ trunk/ java2014/Server2048/src/solver/MazeSolver.java
@@ -1,7 +1,26 @@
 package solver;
 
+import java.util.ArrayList;
+
+import maze.Maze;
+import maze.MazeDistance;
+import maze.MazeDomain;
+import model.algorithms.Action;
+import model.algorithms.a_star.Astar;
+
 public class MazeSolver {
 	
-	p
-	Maze maze = new Maze
+	private int[][] data;
+	
+	public MazeSolver(int[][] data){
+		this.data = data;
+	}
+	
+	public ArrayList<Action> getTheCheese(){
+		Maze maze  = new Maze(data);
+		Astar as = new Astar(new MazeDomain(maze), new MazeDistance(), new MazeDistance());
+		ArrayList<Action> actions = as.search(maze.getStartState() , maze.getGoalState());
+		return actions;
+		
+	}
 }
